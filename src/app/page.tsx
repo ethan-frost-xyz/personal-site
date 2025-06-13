e2f93dde8c36aa4c45f4b496163315f1272c6f91
ethan-frost-xyz <ethanfrostbvt@gmail.com>
2025-06-13 19:26:24 -0400

    deleted old github account key chain and shamone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,10 @@ export default function HomePage() {
     <section className="max-w-2xl mt-20 space-y-6">
       <h1 className="text-4xl font-semibold">Hi, I’m Ethan Frost.</h1>
       <p className="text-lg text-neutral-300">
-        stella this is what i have been doing after work.
+        shamone
       </p>
       <p className="text-neutral-400">
-        i am going to make it into a project portfolio website dont judge i just started today after work idk what the fuck i am dooooooing.
-      </p>
-      <p className="text-neutral-400">
-        what are you doing tonight?  
+        shamone again
       </p>
     </section>
   )
