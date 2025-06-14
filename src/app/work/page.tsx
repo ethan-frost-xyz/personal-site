538c08f260215c8fbee070769267c8c034191d8c
ethan-frost-xyz <ethanfrostbvt@gmail.com>
2025-06-14 15:53:04 -0400

    added some disclaimers for viewers so they know its under construction

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
       <section className="max-w-2xl mt-20 space-y-4">
-        <h1 className="text-3xl font-semibold">Coming soon...</h1>
+        <h1 className="text-3xl font-semibold">Coming soon... currently under construction... see about page for some tangible work</h1>
       </section>
     )
   }
